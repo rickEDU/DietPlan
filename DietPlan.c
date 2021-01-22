6032bbebc275d5492239c17b20bd0f7c20815d28
rickEDU <eduardo.h3nriqu3@gmail.com>
2021-01-22 19:16:30 +0000

    mudança no Main

---
+++ DietPlan.c
@@ -0,0 +1,41 @@
+*******************************************************************************
+///                         Ministério da Saúde                             ///
+///                 Centro de Saúde Pública e bem estar                     ///
+///                Departamento de Computação e Tecnologia                  ///
+///               Projeto Sistema de Planejamento de dietas                 ///
+///                  Developed by  @rickEDU - Jan, 2021                     ///
+*******************************************************************************
+
+#include <stdio.h>
+
+void resulmo(void);
+
+int main(void) {
+  resulmo();
+  return 0;
+
+}
+
+
+void resulmo(void) {
+  printf("\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///                         Ministério da Saúde                             ///\n");
+  printf("///                 Centro de Saúde Pública e bem estar                     ///\n");
+  printf("///               Departamento de Computação e Tecnologia                   ///\n");
+  printf("///           Projeto DietPlan: Sistema de Planejamento de dietas           ///\n");
+  printf("///                Developed by  @rickEDU - Jan, 2021                       ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///     = = = = = DietPlan: Sistema de Planejamento de dietas = = = = =     ///\n");
+  printf("///                                                                         ///\n");
+  printf("///  Programa com o intúito de diminuir as taxas de obesidade no Brasil,    ///\n");
+  printf("///  por meio da melhoria da alimentação dos brasileiros com a ajuda de     ///\n");
+  printf("///  nutricionistas auxiliados pelo o programa, facilitando ao acesso a     ///\n");
+  printf("///  educação alimentar a população mais carente.                           ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("\n");
+}
